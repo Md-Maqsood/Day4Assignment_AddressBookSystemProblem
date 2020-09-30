09ea944505c1abd3a312c8c543814b66ec8980e5
maqsood <maqsoodhamd37@gmail.com>
2020-09-30 15:51:42 +0530

    [maqsood] update solved uc1

--- AddressBook.java
+++ AddressBook.java
@@ -1,5 +1,82 @@
 public class AddressBook{
-  public static void main(String[] args){
-    System.out.println("Welcome to Address Book Program");
-  }
+	public static void main(String[] args) {
+		Contact contact=new Contact("Paul", "Allen", "52, Baker's Street", "New York", "California", 832110, 1234567890,"paul@cap.com");
+		System.out.println("New Contact has been create in address book.\n"+contact);
+	}	
+}
+
+class Contact{
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private int zip;
+	private long phoneNumber;
+	private String email;
+	public Contact(String firstName, String lastName, String address, String city, String state, int zip,
+			long phoneNumber,String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+		this.email=email;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getZip() {
+		return zip;
+	}
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "Contact: "+firstName + lastName+ ", "+address+", "+city+", " +state+ ", "+", " +zip+ ", "+ phoneNumber + "\n"+email+".";
+	}
+	
 }
